45feb5d98f827c87d10c62b66b321efa779360f3
amir mekkaoui <amirmekk@outlook.com>
2021-09-18 00:10:43 +0100

    fully functional webview the back button and progress indicator

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'dart:async';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
     );
   }
 }
@@ -28,40 +31,87 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   DateTime? currentBackPressTime;
   bool _isLoading = false;
+  late WebViewController _controller;
+  double progress = 0;
+
+  final Completer<WebViewController> _completerController =
+      Completer<WebViewController>();
+
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
+  Future<bool> _goBack(BuildContext context) async {
+    if (await _controller.canGoBack()) {
+      _controller.goBack();
+      return Future.value(false);
+    } else {
+      //switch leave or stay in the app in case there is no history to go back to
+      return Future.value(true);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    Future<bool> _onBackButtonPressed() {
-      DateTime now = DateTime.now();
-      if (currentBackPressTime == null ||
-          now.difference(currentBackPressTime!) > Duration(seconds: 2)) {
-        currentBackPressTime = now;
-        Fluttertoast.showToast(msg: 'Press the back button again to exit');
-        return Future.value(false);
-      }
-      return Future.value(true);
-    }
-
     return SafeArea(
-      child: Scaffold(
-        // appBar: AppBar(
-        //   title: Text(widget.title),
-        // ),
-        body: WillPopScope(
-          onWillPop: _onBackButtonPressed,
-          child: WebView(
-            onPageStarted: (url) {
-              setState(() {
-                _isLoading = true;
-              });
-            },
-            onPageFinished: (url) {
-              setState(() {
-                _isLoading = false;
-              });
-            },
-            initialUrl: 'https://flutter.dev',
-            javascriptMode: JavascriptMode.unrestricted,
+      child: WillPopScope(
+        onWillPop: () => _goBack(context),
+        child: Scaffold(
+          appBar: AppBar(
+            // add title if wanted
+            //title: Text("Flutter"),
+            leading: IconButton(
+                onPressed: () async {
+                  if (await _controller.canGoBack()) {
+                    _controller.goBack();
+                  }
+                },
+                icon: Icon(Icons.arrow_back_ios)),
+            actions: [
+              IconButton(
+                onPressed: () {
+                  _controller.reload();
+                },
+                icon: Icon(
+                  Icons.refresh,
+                ),
+              ),
+            ],
+          ),
+          body: Column(
+            children: [
+              progress == 1
+                  ? SizedBox()
+                  : LinearProgressIndicator(value: progress),
+              Expanded(
+                child: WebView(
+                  onPageStarted: (url) {
+                    setState(() {
+                      _isLoading = true;
+                    });
+                  },
+                  onPageFinished: (url) {
+                    setState(() {
+                      _isLoading = false;
+                    });
+                  },
+                  initialUrl: 'https://flutter.dev',
+                  javascriptMode: JavascriptMode.unrestricted,
+                  onWebViewCreated: (WebViewController webViewController) {
+                    _completerController.future
+                        .then((value) => _controller = value);
+                    _completerController.complete(webViewController);
+                  },
+                  onProgress: (p) {
+                    setState(() {
+                      this.progress = p / 100;
+                    });
+                  },
+                ),
+              ),
+            ],
           ),
         ),
       ),
